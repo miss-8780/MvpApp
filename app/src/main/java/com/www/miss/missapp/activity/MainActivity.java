857cd332e4eab8f87e777f41b6d8d588aa2f1813
summer9 <miss_8786@163.com>
2017-08-22 18:17:21 +0800

    上传本地项目

---
+++ app/src/main/java/com/www/miss/missapp/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.www.miss.missapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.www.miss.missapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
